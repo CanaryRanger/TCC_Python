914aac5fcdd60cc4cc4dd7f3d1e35456b76bb6d1
Joao Vitor Nogueira Simoe <joao.simoe@lojastorra.com.br>
2025-07-01 21:00:57 -0300

    Heroku release v1.2

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.layout = get_main_layout()
 register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
 
 """ # Rodar o servidor
